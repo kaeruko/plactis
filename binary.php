8ab090420f100ce8aee302f9c2182264bdbb4676
横田　里奈子 <yokota.rinako@applica.co.jp>
2014-04-30 11:52:43 +0900

    first commit

---
+++ binary.php
@@ -0,0 +1,32 @@
+<?php
+$args = array_slice($argv, 1);
+foreach ($args as $arg) {
+    $ret = toBinary($arg);
+    $binary =  str_pad($ret, 8, "0", STR_PAD_LEFT);
+//   echo "decimal[$arg] binary[$binary]\n";
+echo "$binary\n";
+    // $ret = toDecimal($binary);
+    // echo $ret;
+
+}
+
+function toBinary($num){
+    while(true)
+    {
+        $remainder = $num%2;
+        $num = ($num - $remainder) /2;
+        $ans[] = $remainder;
+        if($num < 1)break;
+    }
+    return implode(array_reverse($ans),"");
+}
+
+function toDecimal($num){
+    $num = str_split($num);
+    $a = array_reverse($num);
+    foreach ($a as $key => $val) {
+        $ans += pow(2, $key) * $val;
+    }
+    echo $ans;
+}
+
