6d728a3ecbe4491593d18f49553f7aea8b1a8304
kaeruko <mail@cloxs.jp>
2014-05-09 22:00:11 +0900

    読み込み処理追加

--- c/changebinary.c
+++ c/changebinary.c
@@ -8,19 +8,35 @@ int main(int argc, char *argv[])
     FILE *fp;
     //file存在チェック用
     struct stat st;
+    //読み込みバッファ。binaryが入ってる
+    int inbuf;
+    //書き込みバッファ
+    int outbuf;
 
     //引数はファイル名を指定
     char *file = argv[1];
 
-    fp = fopen(file, "wb");
+    printf("戻り値チェック: %d\n", stat(file, &st));
 
-    printf("戻り値チェック: %c\n", stat(file, &st));
-    //ファイル存在チェック
+    //ファイル存在チェック。エラーの場合は-1
     if(stat(file, &st) != 0)
     {
         printf("%sは存在しません\n", file);
         return 0;
     }
+
+    //バイナリ形式で開く
+    fp = fopen(file, "rb");
+
+    //1行読み込む
+    fread(&inbuf, sizeof( file ), 1, fp);
+
+    //バイナリ形式で出力
+    printf("%d\n", inbuf);
+
+    //inbufを別のファイルに書き込む
+
+    fclose(fp);
 
     return 0;
 }
