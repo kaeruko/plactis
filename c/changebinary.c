ba4a1a1697329a20d9c714691fa2d4122748b076
kaeruko <mail@cloxs.jp>
2014-05-09 16:37:46 +0900

    ファイル存在チェック追加

--- c/changebinary.c
+++ c/changebinary.c
@@ -1,17 +1,26 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/stat.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 
     FILE *fp;
+    //file存在チェック用
+    struct stat st;
+
     //引数はファイル名を指定
-    char file[256] = argv[1];
-    //引数にファイルを指定する
-    printf("%s\n", argv[1]);
+    char *file = argv[1];
 
     fp = fopen(file, "wb");
 
+    printf("戻り値チェック: %c\n", stat(file, &st));
+    //ファイル存在チェック
+    if(stat(file, &st) != 0)
+    {
+        printf("%sは存在しません\n", file);
+        return 0;
+    }
 
     return 0;
 }
