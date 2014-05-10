5e49e65a53f958a348c7c117b7c7889f39bbd297
yoya <yoya@awm.jp>
2014-05-10 10:00:34 +0900

    initiaize for $ans

--- php/decimal.php
+++ php/decimal.php
@@ -9,6 +9,7 @@ $ans = 0;
 function toDecimal($num){
     $num = str_split($num);
     $a = array_reverse($num);
+    $ans = 0;
     foreach ($a as $key => $val) {
         $ans += pow(2, $key) * $val;
     }
