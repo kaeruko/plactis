51a11b0f34f9db18cef9277635883c3252802674
kaeruko <mail@cloxs.jp>
2014-05-13 15:27:58 +0900

    width, height変数追加

--- c/imagesize.c
+++ c/imagesize.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     //file存在チェック用
     struct stat st;
     //読み込みバッファ。binaryが入ってる
-    unsigned char buf[100000000];
+    unsigned char buf[1000000];
     int count;
     int width;
     int height;
@@ -26,9 +26,9 @@ int main(int argc, char *argv[])
     //バイナリ形式で開く
     fp = fopen(file, "rb");
 
-    count = fread(&buf, sizeof( unsigned char ), 100000000, fp);
+    count = fread(&buf, sizeof( unsigned char ), 1000000, fp);
 
-    printf("%d\n", count);
+    printf("count %d\n", count);
 
     width = (buf[7] * 256) + (buf[6] * 1);
     height = (buf[9] * 256) + (buf[8] * 1);
