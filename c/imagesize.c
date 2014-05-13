705d4d17de51ad68030a0bf6e6fc09653cd2dd45
kaeruko <mail@cloxs.jp>
2014-05-13 14:11:27 +0900

    バッファのメモリを100000000にする

--- c/imagesize.c
+++ c/imagesize.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     //file存在チェック用
     struct stat st;
     //読み込みバッファ。binaryが入ってる
-    unsigned char buf[10000];
+    unsigned char buf[100000000];
     int count;
 
     //引数はファイル名を指定
@@ -24,7 +24,9 @@ int main(int argc, char *argv[])
     //バイナリ形式で開く
     fp = fopen(file, "rb");
 
-    count = fread(&buf, sizeof( unsigned char ), 10000, fp);
+    count = fread(&buf, sizeof( unsigned char ), 100000000, fp);
+
+    printf("%d\n", count);
 
     printf("width: %d \n", (buf[7] * 256) + (buf[6] * 1));
     printf("height %d \n", (buf[9] * 256) + (buf[8] * 1));
