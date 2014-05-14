3ddd06dd46acffa1717440cbbb0b0cad1021b962
kaeruko <mail@cloxs.jp>
2014-05-14 15:24:39 +0900

    表示方法変更

--- php/refer.php
+++ php/refer.php
@@ -10,11 +10,12 @@ foreach($allhtmls as $id => $html){
     $refers = array_merge( refer($html), $refers);
 }
 
-var_dump(array_count_values($refers));
-
-
-//asort($refers);
-//var_dump($refers);
+$count_refers = array_count_values($refers);
+foreach ($count_refers as $html => $count) {
+    if($count == 1){
+        print($html)."\n";
+    }
+}
 
 function refer( $html ){
     $contents = file_get_contents($html);
