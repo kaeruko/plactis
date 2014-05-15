81d9c3670789193fccc14de233c95d15790da61c
kaeruko <mail@cloxs.jp>
2014-05-15 14:37:56 +0900

    ディレクトリを引数指定に

--- motomi/refer.php
+++ motomi/refer.php
@@ -1,10 +1,9 @@
 <?php
 
-//const HTMLDIR = "./motomi/";
-const HTMLDIR = "/home/www/test/motomi/";
+$workdir = $argv[1];
 
 //全htmlを取得
-$allhtmls = array_filter(explode("\n",shell_exec("ls ".HTMLDIR."/*/*htm" )), 'strlen');
+$allhtmls = array_filter(explode("\n",shell_exec("ls ".$workdir."/*/*htm" )), 'strlen');
 
 //参照先リンクを保存
 foreach($allhtmls as $id => $html_full){
@@ -13,27 +12,33 @@ foreach($allhtmls as $id => $html_full){
     $dir = $tmp[count($tmp)-2];
     $html = $tmp[count($tmp)-1];
     $refers[] = $dir."/".$html;
-    $refers = array_merge( refer($dir, $html), $refers);
+    $refers = array_merge( refer($dir, $html, $workdir), $refers);
 }
 
 asort($refers);
 //var_dump($refers);
 $count_refers = array_count_values($refers);
 asort($count_refers);
-var_dump($count_refers);
+//var_dump($count_refers);
 foreach ($count_refers as $html => $count) {
     if($count == 1){
        print($html)."\n";
     }
 }
 
-function refer( $dir, $html ){
-    $contents = file_get_contents( HTMLDIR . $dir."/".$html);
+function refer( $dir, $html , $workdir){
+    $contents = file_get_contents( $workdir . $dir."/".$html);
     preg_match_all('/.+<?\shref=[\'|"](.*?)[\'|"].*/', $contents, $matches, PREG_PATTERN_ORDER);
 //var_dump($matches);
     $add_dir = function($url) use( $dir, $html) {
+
+        // if($url === $html)
+        //     print($url." ".$html."\n");
+
        if(strpos($url, "http", 0) === 0 ||
         strrpos($url, "htm", 0) === false || $url === $html) return false;
+
+
         return $dir."/".$url;
     };
 
