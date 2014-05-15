f45b706f59062f547f1507adcf9bb540bbfead29
kaeruko <mail@cloxs.jp>
2014-05-15 11:11:50 +0900

    外部リンクとメールタグとページ内リンクは除外

--- motomi/refer.php
+++ motomi/refer.php
@@ -16,19 +16,23 @@ foreach($allhtmls as $id => $html_full){
 }
 
 $count_refers = array_count_values($refers);
+asort($count_refers);
+//var_dump($count_refers);
 foreach ($count_refers as $html => $count) {
-    if($count == 1){
-        print($html)."\n";
+    if($count == 1 && strpos($html, "http", 0) == 0){
+       print($html)."\n";
     }
 }
 
 function refer( $dir, $html ){
     $contents = file_get_contents( HTMLDIR . $dir."/".$html);
     preg_match_all('/<a href=[\'|"](.*?)[\'|"].*/i', $contents, $matches, PREG_PATTERN_ORDER);
-    $add_dir = function($html) use( $dir) {
-//        if(strpos($html, "http", 0) == 0 || strpos($html, "mail", 0) == 0)return false;
-//        return $dir."/".$html;
-        return $html;
+    $add_dir = function($url) use( $dir, $html) {
+        // if($url == "f")echo $dir.$html."\n";
+       if(strpos($url, "http", 0) === 0 ||
+        strpos($url, "mail", 0) === 0 ||
+        strpos($url, "#", 0) === 0) return false;
+        return $url;
     };
 
     return array_filter(array_map($add_dir, $matches[1]));
