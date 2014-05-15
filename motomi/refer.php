9e56d646b4717d18b8a771c881767a1e9f97c986
kaeruko <mail@cloxs.jp>
2014-05-15 14:19:35 +0900

    自己参照を弾く

--- motomi/refer.php
+++ motomi/refer.php
@@ -1,7 +1,7 @@
 <?php
 
-const HTMLDIR = "./motomi/";
-// const HTMLDIR = "/home/www/test/motomi/";
+//const HTMLDIR = "./motomi/";
+const HTMLDIR = "/home/www/test/motomi/";
 
 //全htmlを取得
 $allhtmls = array_filter(explode("\n",shell_exec("ls ".HTMLDIR."/*/*htm" )), 'strlen');
@@ -16,6 +16,8 @@ foreach($allhtmls as $id => $html_full){
     $refers = array_merge( refer($dir, $html), $refers);
 }
 
+asort($refers);
+//var_dump($refers);
 $count_refers = array_count_values($refers);
 asort($count_refers);
 var_dump($count_refers);
@@ -28,9 +30,10 @@ foreach ($count_refers as $html => $count) {
 function refer( $dir, $html ){
     $contents = file_get_contents( HTMLDIR . $dir."/".$html);
     preg_match_all('/.+<?\shref=[\'|"](.*?)[\'|"].*/', $contents, $matches, PREG_PATTERN_ORDER);
+//var_dump($matches);
     $add_dir = function($url) use( $dir, $html) {
        if(strpos($url, "http", 0) === 0 ||
-        strrpos($url, "htm", 0) === false ) return false;
+        strrpos($url, "htm", 0) === false || $url === $html) return false;
         return $dir."/".$url;
     };
 
