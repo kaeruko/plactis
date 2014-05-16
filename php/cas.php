d6767e499b170cb261845102f10501d252a81d0b
kaeruko <mail@cloxs.jp>
2014-05-16 18:00:07 +0900

    cas対応

--- php/cas.php
+++ php/cas.php
@@ -6,19 +6,29 @@ $memcache->addServer('localhost', 11211);
 
 //setの場合
 if(isset($options["s"])){
-    $count = $memcache->get($options["s"]);
-    $count =  $count ? ++$count : 1;
-    $memcache->set($options["s"], $count, 50);
-    print "{$options["s"]}を登録しました {$count}\n";
+    $cache_key = $options["s"];
+    $count = $memcache->get($cache_key, null, $cas_token);
+//var_dump($cas_token);
+    if($memcache->getResultCode() == Memcached::RES_NOTFOUND){
+        $count = 1;
+        $memcache->add($cache_key, $count, 50);
+        print "{$cache_key}を登録しました\n";
+    }else{
+        ++$count;
+        $memcache->cas($cas_token, $cache_key, $count);
+        print "{$cache_key}を更新しました {$count}\n";
+    }
 }
 
+//deleteの場合
 if(isset($options["d"])){
-    $count = $memcache->get($options["d"]);
+    $cache_key = $options["d"];
+    $count = $memcache->get($cache_key);
     if($count){
-        $memcache->delete($options["d"]);
-        print "{$options["d"]}を削除しました {$count}\n";
+        $memcache->delete($cache_key);
+        print "{$cache_key}を削除しました {$count}\n";
     }else{
-        print "{$options["d"]}ありません\n";
+        print "{$cache_key}はありません\n";
     }
 }
 
