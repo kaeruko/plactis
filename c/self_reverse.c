c36ddd5567b70c7d534b73b8cb0adc048d42b6ac
kaeruko <mail@cloxs.jp>
2014-05-27 17:36:40 +0900

    指定の要素のみ表示する関数、追加削除freeする関数を追加

--- c/self_reverse.c
+++ c/self_reverse.c
@@ -9,6 +9,13 @@ struct list {
 
 struct list *add( struct list *head, char name[] );
 void show( struct list *head );
+void free_list( struct list *head );
+
+void show_target( struct list *head , int target);
+
+void insert( struct list *head , int target);
+
+void delete_list( struct list *head , int target);
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +25,7 @@ int main(int argc, char *argv[])
 
     int i ;
     int count = 3;
+    int num = 0;
 
     head = NULL;
 
@@ -26,7 +34,12 @@ int main(int argc, char *argv[])
         scanf("%s", name);
         head = add(head, name);
     }
+    // show_target(head, num);
+    // insert(head, num);
+    delete_list(head, num);
     show(head);
+    free(head);
+
     return 0;
 }
 
@@ -35,6 +48,9 @@ struct list *add( struct list *head, char name[] ){
     p = (struct list *) malloc(sizeof(struct list));
     strcpy(p->name, name);
     p->next = NULL;
+
+// printf("name = %s p = %p  \n", name,  p);
+
     if(head == NULL){
         p->next = head;
         return p;
@@ -44,17 +60,78 @@ struct list *add( struct list *head, char name[] ){
         p2 = p2->next;
     }
     p2->next = p;
-printf("name = %s next = %p head = %p  \n", name, &head->next, &head);
     return head;
-
  }
 
 
 void show( struct list *head ){
     struct list *p ;
     while(head != NULL){
-printf("%s\n", head->name);
+printf("p = %p c = %s\n", p, head->name);
         p = head->next;
         head = p;
     }
+}
+
+void show_target( struct list *head , int target){
+    struct list *p ;
+    int i = 0;
+    while(head != NULL){
+        if(i == target){
+            printf("%s\n", head->name);
+        }
+        p = head->next;
+        head = p;
+        i++;
+    }
+}
+
+void insert( struct list *head , int target){
+    struct list *add;
+    add = (struct list *)malloc(sizeof(struct list));
+
+    struct list *p, *prev;
+    int i = 0;
+    while(head != 0){
+        prev = head;
+        p = head->next;
+        head = p;
+                            // printf("i = %d name= %s prev = %p current= %p \n", i, head->name, prev, head);
+        if( i == target ){
+            add->next = head;
+            strcpy(add->name, "add");
+            prev->next = add;
+                            // printf("* \n");
+            break;
+        }
+        i++;
+    }
+}
+
+void delete_list( struct list *head , int target){
+    struct list *add;
+    add = (struct list *)malloc( sizeof( struct list )  );
+    struct list *p, *prev;
+    int i = 0;
+    while(head != 0){
+        prev = head;
+        p = head->next;
+        head = p;
+        if( i == target){
+            prev->next = head->next;
+            free(head);
+            break;
+        }
+        i++;
+    }
+}
+
+void free_list( struct list *head ){
+    struct list *p;
+    while(head != NULL){
+        p = head->next;
+        free(head);
+        head = p;
+    }
+
 }
