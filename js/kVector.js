3c94f2b5a8ef399c8fdb986f16f033caf986f020
kaeruko <mail@cloxs.jp>
2014-07-24 18:01:47 +0900

    bug fix relativeLen

--- js/kVector.js
+++ js/kVector.js
@@ -25,9 +25,13 @@ kVector.prototype.setStartPoint = function(e){
  */
 kVector.prototype.relativeLen = function(start, end){
     var ret = [
-        Math.abs(end[0] - start[0]),
-        Math.abs(end[1] - start[1])
+        // Math.abs(end[0] - start[0]),
+        // Math.abs(end[1] - start[1])
+        end[0] - start[0],
+        end[1] - start[1]
     ]
+console.debug( "end.x: ", end[0], " st:.x:", start[0] , end[0] - start[0] );
+console.debug( "relativeLen:", ret  );
     return ret;
 }
 
@@ -46,7 +50,7 @@ kVector.prototype.setEndPoint = function(e){
     this.kc.stroke(this.startPoint, this.endPoint);
 
     //相対ベクトルを表示
-    var veclen = this.relativeLen(this.endPoint, this.startPoint ) ;
+    var veclen = this.relativeLen(this.startPoint, this.endPoint ) ;
     this.showlenText(veclen, this.endPoint);
 
     //ベクトル1が設定済みの場合は、足した後のベクトルを表示する
@@ -56,9 +60,10 @@ kVector.prototype.setEndPoint = function(e){
 
         var added = this.addVector(this.vec1len, this.vec2len);
         var addedPoint = this.addVectorPoint( added, this.vector1, this.vector2);
-// console.debug(added, addedPoint);
+// console.debug(" added: ", added, " addedPoint:", addedPoint[0], " ~ ", addedPoint[1]);
         this.showlenText(added, addedPoint[1]);
         this.kc.stroke(addedPoint[0], addedPoint[1]);
+        this.kc.fillCircle(addedPoint[1], 0.05, Math.PI * 2, "red", true);
 
         this.startPoint = 0;
         this.endPoint = 0;
@@ -79,12 +84,14 @@ kVector.prototype.addVectorPoint = function(add, vec1, vec2){
     var start = [  ( f(vec1[0][0]) + f(vec2[0][0]) )  / 2 ,
                    ( f(vec1[0][1]) + f(vec2[0][1]) )  / 2 ];
      var end = [  start[0] + add[0] , start[1] + add[1] ];
+
     return [start, end];
 }
 
 kVector.prototype.addVector = function(vec1len, vec2len){
     //width, hight
    var add = [
+        // Math.abs(vec1len[0] + vec2len[0]), Math.abs(vec1len[1] + vec2len[1])
         vec1len[0] + vec2len[0], vec1len[1] + vec2len[1]
     ];
     return add;
